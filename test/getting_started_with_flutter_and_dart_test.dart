e0ed7c5078b1792001dcf7b60286407d578983fb
Velma Kimani <stanvelma16@gmail.comm>
2022-08-22 19:04:44 +0300

    other files

---
+++ test/getting_started_with_flutter_and_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:getting_started_with_flutter_and_dart/getting_started_with_flutter_and_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
